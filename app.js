e880fdd72dc8467635e8d2ef0f02da2352754abb
vinay_prajapati_ <vinayprajapat586@gmail.com>
2024-12-07 18:27:56 +0530

    mis

--- app.js
+++ app.js
@@ -16,6 +16,49 @@ const cont = React.createElement("div", {}, [
 ]);
 
 // Render React elements
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(cont);
-console.log("Hello ");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(cont);
+// console.log("Hello ");
+
+const h1 = React.createElement("h1", {}, "Heading1");
+const h2 = React.createElement("h1", {}, "Heading2");
+const h3 = React.createElement("h1", {}, "Heading3");
+
+const jx = (
+  <div>
+    <h1>Heading1 of jsx</h1>
+    <h1>Heading2 of jsx</h1>
+    <h1>Heading3 of jsx</h1>
+  </div>
+);
+
+const Fc = () => {
+  return (
+    <div>
+      <h1>Heading1 of jsxx</h1>
+      <h1>Heading2 of jsxx</h1>
+      <h1>Heading3 of jsxx</h1>
+    </div>
+  );
+};
+
+
+const Fcv = () => {
+  return (
+    <div>
+      <Fc/>
+      <h2>Heading1 of jsxx</h2>
+      <h2>Heading2 of jsxx</h2>
+      <h2>Heading3 of jsxx</h2>
+    </div>
+  );
+};
+
+
+
+
+
+const cdiv = React.createElement("div", {}, [h1, h2, h3]);
+
+const root = ReactDOM.createRoot(document.getElementById("k"));
+root.render([cdiv, jx, <Fcv />]);
